b9523d01f001c2bfa5ba93e1cb9445b039f63a6d
Thiago Porto <62581842+porthiago@users.noreply.github.com>
2022-08-13 12:12:37 -0300

    Update nodemailer.js

--- 30. Envio de E-mails/aulas/classe/src/nodemailer.js
+++ 30. Envio de E-mails/aulas/classe/src/nodemailer.js
@@ -2,21 +2,22 @@ const nodemailer = require('nodemailer');
 const handlebars = require('nodemailer-express-handlebars');
 
 // let transportador = nodemailer.createTransport({
-//   host: 'smtp.mailgun.org',
+//   host:process.env.TRANSPORTADOR_HOST,
 //   port: 587,
 //   secure: false, // true for 465, false for other ports
 //   auth: {
-//     user: 'postmaster@sandboxf033b2ae88674908a59c94cb13119c71.mailgun.org',
-//     pass: '310ba3c68974e2d7f1450db18a7a98c0-77985560-449091e5' // generated ethereal password
+//     user: process.env.TRANSPORTADOR_AUTH_USER,
+//     pass:  process.env.TRANSPORTADOR_AUTH_PASS // generated ethereal password
 //   }
 // });
 
 const transportador = nodemailer.createTransport({
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.TRANSPORTADOR_HOST,
+  port: 587,
+  secure: false,
   auth: {
-    user: '82def955c84490',
-    pass: 'db24f5384edf6f'
+    user: process.env.TRANSPORTADOR_AUTH_USER,
+    pass: process.env.TRANSPORTADOR_AUTH_PASS
   }
 });
 
